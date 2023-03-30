4aae0722b919742a31207eb575243f7ae597cf9c
Anthony <cooperkl317@gmail.com>
2023-03-29 17:06:10 -0700

    add test

--- caeserCipher.test.js
+++ caeserCipher.test.js
@@ -3,3 +3,7 @@ const cipher = require('./caeserCipher');
 test('aaa shift 1 to bbb', () => {
     expect(cipher('aaa', 2)).toBe('ccc');
 });
+
+test('waze shift 10 to bbb', () => {
+    expect(cipher('waze', 10)).toBe('gkjo');
+});
