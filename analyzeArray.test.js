443c9c718568cd78082f38efdabb33503507bd81
Anthony <cooperkl317@gmail.com>
2023-03-29 17:36:47 -0700

    add tests

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -4,3 +4,7 @@ test('attributes of array', () => {
     expect(analyzeArray([3, 2, 1])).toStrictEqual({ average: 2, min: 1, max: 3, length: 3});
 });
 
+test('attributes of array', () => {
+    expect(analyzeArray([-3, 20, 13, -4])).toStrictEqual({ average: 6.5, min: -4, max: 20, length: 4});
+});
+
