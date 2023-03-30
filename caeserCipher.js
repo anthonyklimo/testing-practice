1a281b96d09d74758b97ae0f51f94f6772896d33
Anthony <cooperkl317@gmail.com>
2023-03-29 17:05:58 -0700

    finalize function

--- caeserCipher.js
+++ caeserCipher.js
@@ -1,28 +1,30 @@
 function cipher(word, shift) {
     const wordArray = Array.from(word);
     const alphabet = Array.from('abcdefghijklmnopqrstuvwxyz');
-
-    while(parseInt(shift) > 26) {
+    const wordIndeces = [];
+    const shiftedWord = [];
+    // turn shift argument into a value usable for array indeces
+    shift = parseInt(shift);
+    while(shift > 26) {
         shift = shift - 26;
     };
 
-    const shiftedAlphabet = alphabet.concat(alphabet.splice(0, shift));
-    const wordIndeces = [];
+    function shiftIndex(num) {     
+        if (num + shift > 26) {
+            return (num + shift - 26);
+        }
+        return (num + shift);
+    }
 
     wordArray.forEach(character => {
-        wordIndeces.push(alphabet.indexOf(character));
+        wordIndeces.push(shiftIndex(alphabet.indexOf(character)));
     });
 
-    const shiftedWord = [];
     wordIndeces.forEach(index => {
-        if (index < 0) {
-            shiftedWord.push(shiftedAlphabet[index + 26])
-        };
-
-        shiftedWord.push(shiftedAlphabet[index]);
+        shiftedWord.push(alphabet[index]);
     });
 
-    return shiftedAlphabet;
+    return shiftedWord.join('');
 };
 
 module.exports = cipher;
